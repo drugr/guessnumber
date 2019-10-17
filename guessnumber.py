2213d40c2b1ea2e4b4cedad2d12ce7fae09bb97d
Kunn Liu <abce1202@gmail.com>
2019-10-17 19:15:03 +0800

    v2.0.1

--- guessnumber.py
+++ guessnumber.py
@@ -1,6 +1,8 @@
 import random
+print('--------------------------------')
 start = input('請決定猜數字最小值: ')
 end = input('請決定猜數字最大值: ')
+print('--------------------------------')
 start = int(start)
 end = int(end)
 while True:
@@ -8,9 +10,11 @@ while True:
 		r = random.randint(start, end)
 		count = 0
 		while True:
-			end = str(end)
 			start = str(start)
-			num = input('請輸入' + start +'到' + end + '中間的數字: ')#裡面是字串喔~~~~!!!!
+			end = str(end)
+			num = input('請輸入' + start + '到' + end + '中間的數字: ')#裡面是字串喔~~~~!!!!
+			start = int(start)
+			end = int(end)
 			count += 1
 			num = int(num)
 			if num == r:
@@ -21,9 +25,12 @@ while True:
 				print('你猜得太大囉~')
 			elif num < r:
 				print('你猜得太小囉~')
-			print('總共猜了', count, '次喔!!', r)
+			print('總共猜了', count, '次喔!!')
+			print('--------------------------------')
+		break
 	else:
 		print('最大值要大於最小值喔!!')
+		print('--------------------------------')
 		start = input('請決定猜數字最小值: ')
 		end = input('請決定猜數字最大值: ')
 		start = int(start)
